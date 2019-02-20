24a73dffb86fd34026210e78a5ebe0ca63d75b0d
HuLuoQian <494843897@qq.com>
2019-02-20 10:56:41 +0800

    1.6 inputs

---
+++ components/QuShe-inputs/inputs.vue
@@ -0,0 +1,444 @@
+<template>
+	<view class="width100">
+		<view :class="item.type&&item.type=='pics'?'flex_column':'flex_row'" class="width100 padding2vh3vw box-sizing-border-box"
+		 v-for="(item, index) in inputs" :key="index">
+			<view :class="item.type&&item.type=='pics'?'flex_row_none_c':'flex_row_e_c'" class="input_title" :style="{'fontSize': fontSize + 'vh', 'color': fontColor}">
+				<view class="fontSize1Point8vh fontColorF1505C" v-if="item.type!='pics'&&!item.ignore">*</view>{{item.title || ''}}:
+			</view>
+			<view class="width100 padding1vh3vw box-sizing-border-box overflow_x_scroll" v-if="item.type&&item.type=='pics'">
+				<view class="flex_row width100">
+					<view class="flex_column_c_c padding1vh" v-for="(picsItem, picsIndex) in item.picsArray" :key="picsIndex">
+						<view class="flex_row_c_c pic_view" @tap="!picsObj[onLoadData + index + onLoadData + picsIndex]?chooseImg(index, picsIndex): ''">
+							<image :src="picsObj[onLoadData + index + onLoadData + picsIndex]" mode="aspectFill" class="picsItem_pic" v-if="picsObj[onLoadData + index + onLoadData + picsIndex]"></image>
+							<view class="fontSize2Point1vh fontColorADADAD" v-else>+</view>
+							<view class="clearView" v-if="picsObj[onLoadData + index + onLoadData + picsIndex]" @tap.stop.prevent="clearPic(index, picsIndex)">
+								<uni-icon type="clear" color="#f5105c" size="3" />
+							</view>
+						</view>
+						<view class="flex_row_c_c marginTop1vh picsItem_title" v-if="picsItem.title">
+							<view class="fontSize1Point8vh fontColorF1505C" v-if="!picsItem.ignore">*</view>{{picsItem.title}}
+						</view>
+					</view>
+				</view>
+			</view>
+			<view class="input_item overflow_x_scroll" v-else-if="item.type&&item.type=='radio'">
+				<radio-group @change="inputs_change($event, index)" class="width100 flex_row_none_c">
+					<label class="marginItemRight2vw fontSize1Point8vh fontColor666666 flex_row_none_c" v-for="(radioItem, radioIndex) in item.radioArray"
+					 :key="radioIndex">
+						<view>
+							<radio :value="radioItem.value"/>
+						</view>
+						<view class="flex_row_none_c radioItem_name">{{radioItem.name}}</view>
+					</label>
+				</radio-group>
+			</view>
+			<view class="input_item overflow_x_scroll" v-else-if="item.type&&item.type=='checkbox'">
+				<checkbox-group @change="inputs_change($event, index)" class="width100 flex_row_none_c">
+					<label class="marginItemRight2vw fontSize1Point8vh fontColor666666 flex_row_none_c" v-for="(checkboxItem, checkboxIndex) in item.checkboxArray"
+					 :key="checkboxIndex">
+						<view>
+							<checkbox :value="checkboxItem.value"/>
+						</view>
+						<view class="flex_row_none_c radioItem_name">{{checkboxItem.name}}</view>
+					</label>
+				</checkbox-group>
+			</view>
+			<view class="input_item" v-else>
+				<input :type="item.inputType||'text'" value="" @input="inputs_change($event, index)" :placeholder="item.placeholder||'请输入' + item.title"
+				 class="width100 borderBottom1pxf2f2f2" :style="{'fontSize': input_fontSize + 'vh'}" />
+			</view>
+		</view>
+		<view class="flex_row width100 padding2vh3vw box-sizing-border-box" v-if="ifCode">
+			<view class="flex_row_e_c input_title" :style="{'fontSize': fontSize + 'vh', 'color': fontColor}">
+				<view class="fontSize1Point8vh fontColorF1505C">*</view>验证码:
+			</view>
+			<view class="input_item">
+				<input type="text" value="" v-model="userCode" placeholder="请输入验证码" class="width100 borderBottom1pxf2f2f2" :style="{'fontSize': input_fontSize + 'vh'}" />
+			</view>
+		</view>
+		<view class="flex_row_between_c padding1vh3vw box-sizing-border-box">
+			<view class="flex_row_c_c fontSize1Point8vh fontColor666666" v-if="ifRule">
+				<label class="flex_row_c_c">
+					<switch :checked="Igree" @change="IgreeFc" type="checkbox" />我已阅读并同意</label>
+				<view class="fontColor007AFF" @tap="openRuleFc(ruleItem.value)" v-for="(ruleItem, ruleIndex) in ruleArray" :key="ruleIndex">{{ruleIndex==0?ruleItem.name:'、' + ruleItem.name}}</view>
+			</view>
+			<button type="primary" size="mini" v-if="ifCode" :disabled="startCode" @tap="!startCode?getCode():''">{{startCode?codeCount + 's后重新获取':'获取验证码'}}</button>
+		</view>
+		<button type="primary" class="margin2vh3vw" @tap="activeFc">{{activeName}}</button>
+	</view>
+</template>
+
+<script>
+	import _app from '@/util/appJS.js';
+	import SendMsg from '@/util/SendMsg-Manager.js';
+	import md5 from '@/util/md5.js';
+	import uniIcon from './uni-icon.vue';
+	export default {
+		props: {
+			inputs: { // 需循环遍历的input
+				type: Array,
+				default: null
+			},
+			fontSize: { // title 的font-size
+				type: Number,
+				default: 2.1
+			},
+			fontColor: { // color
+				type: String,
+				default: '#666666'
+			},
+			input_fontSize: { // inputs的font-size
+				type: Number,
+				default: 2.1
+			},
+			ruleArray: {
+				type: Array,
+				default: null
+			},
+			activeName: { // 发送按钮的名字
+				type: String,
+				default: '发送'
+			},
+			ifCode: { // 是否需要验证码
+				type: Boolean,
+				default: false
+			},
+			ifRule: { // 是否需要规则
+				type: Boolean,
+				default: false
+			},
+			onLoadData: { // 数据变量名（+index）
+				type: String,
+				default: 'data_'
+			}
+		},
+		data() {
+			return {
+				code: '',
+				userCode: '',
+				Igree: false,
+				codeCount: 90,
+				startCode: false,
+				picsObj: {}
+			};
+		},
+		methods: {
+			IgreeFc(e) { // 用户是否同意规则
+				this.Igree = e.detail.value;
+			},
+			openRuleFc(value) { // 打开规则页面的父级方法
+				this.$emit('chaildOpenEvent', value);
+			},
+			inputs_change(e, index) { // 用户输入时，根据index赋值
+				this[this.onLoadData + index] = e.detail.value;
+			},
+			getCode() { // 判断是否正确输入手机号后发送验证码并倒计时
+				let _this = this;
+				let phone = '';
+				let d = _this.inputs;
+				for (let i = 0; i < d.length; i++) {
+					if (d[i].phone) {
+						phone = _this[_this.onLoadData + i];
+					}
+				}
+				if (phone && phone != null && !isNaN(phone) && phone.length == 11)
+					_this.sendSMS(phone);
+				else {
+					_app.showToast('请正确输入11位手机号');
+					return;
+				}
+
+				_this.setInterValFunc = setInterval(() => {
+					if (_this.codeCount > 0)
+						--_this.codeCount;
+					else {
+						_this.startCode = false;
+						clearInterval(_this.setInterValFunc);
+						_this.code = '';
+						_this.codeCount = 90;
+					}
+				}, 1000)
+				_this.startCode = true;
+			},
+			sendSMS(phone) { // 生成验证码并发送给手机的方法， 可根据需求在父组件导入(可变通性高)或直接写在此处
+				//生成验证码
+				this.code = '生成的验证码';
+				//发送验证码
+				_app.showToast('发送验证码成功')
+			},
+			activeFc() { // 主功能按钮方法
+				let _this = this;
+				let d = _this.inputs;
+				let inputsDataObj = {};
+				// 先判断 inputs 的值是否为空, 后判断该值是否忽略
+				for (let i = 0; i < d.length; i++) {
+					switch (d[i].type) {
+						case 'pics':
+							for (let j = 0; j < d[i].picsArray.length; j++) {
+								let pic = _this.picsObj[_this.onLoadData + i + _this.onLoadData + j];
+								if (pic) {
+									if (!inputsDataObj[_this.onLoadData + i])
+										inputsDataObj[_this.onLoadData + i] = [];
+									inputsDataObj[_this.onLoadData + i].push(pic);
+								} else {
+									if (d[i].picsArray[j].ignore) {
+										if (!inputsDataObj[_this.onLoadData + i])
+											inputsDataObj[_this.onLoadData + i] = [];
+										inputsDataObj[_this.onLoadData + i].push('');
+									} else {
+										_app.showToast(d[i].picsArray[j].nullErr || d[i].picsArray[j].title + '不能为空');
+										return;
+									}
+								}
+							}
+							break;
+						default:
+							if (!_this[_this.onLoadData + i]) {
+								if (d[i].ignore) {
+									inputsDataObj[_this.onLoadData + i] = '';
+								} else {
+									_app.showToast(d[i].nullErr || d[i].title + '不能为空');
+									return;
+								}
+							} else {
+								inputsDataObj[_this.onLoadData + i] = _this[_this.onLoadData + i];
+							}
+							break;
+					}
+				}
+				// 判断是否需要同意规则，是否填写验证码并判断是否正确
+				if (_this.ifRule)
+					if (!_this.Igree) {
+						let ruleName = '';
+						for(let i = 0; i < _this.ruleArray.length; i++) {
+							ruleName += i==0?_this.ruleArray[i].name:'、' + _this.ruleArray[i].name;
+						}
+						_app.showToast('请先阅读并勾选' + ruleName);
+						return;
+					}
+				if (_this.ifCode) {
+					if (!_this.code) {
+						_app.showToast('请先获取验证码');
+						return;
+					} else if (!_this.userCode) {
+						_app.showToast('填写验证码');
+						return;
+					} else if (_this.userCode !== _this.code) {
+						_app.showToast('验证码不正确');
+						_this.userCode = '';
+						_this.code = '';
+						return;
+					}
+				}
+				// 如果用了图片类型， 则上传并返回数据
+				let pic_promise = [];
+				for (let i = 0; i < d.length; i++) {
+					if (d[i].type && d[i].type == 'pics') {
+						for (let j = 0; j < d[i].picsArray.length; j++) {
+							if (inputsDataObj[_this.onLoadData + i][j]) {
+								pic_promise.push(new Promise(function(reslove, reject) {
+									// push Promise 上传图片到服务器并返回图片在服务器的地址并拼接的方法
+									_app.UpLoadFile(_app.interface.upLoadImg, {}, 'name', inputsDataObj[_this.onLoadData + i][j], function(res) {
+										reslove({
+											index1: i,
+											index2: j,
+											data: res.data
+										}); // index2 基本无用， 若无需求可删
+									});
+								}));
+							} else {
+								pic_promise.push(new Promise(function(reslove, reject) { // 若用户未填此数据并忽略此数据时传空以记录此次数据
+									reslove({
+										index1: i,
+										index2: j,
+										data: ''
+									}); // index2 基本无用， 若无需求可删
+								}));
+							}
+						}
+					}
+				}
+				Promise.all(pic_promise).then(res => { // Promise返回一个图片的数组, 根据res.index1 给 inputsDataObj[_this.onLoadData + res[i].index1] 赋值
+					if (res.length > 0)
+						for (let i = 0; i < res.length; i++) { // 注： 此处根据自己的需求拼接数据   (在下返回数据后的拼接是以 ‘|’ 分隔)
+							/* if(typeof(inputsDataObj[_this.onLoadData + res[i].index1]) != 'string')
+								inputsDataObj[_this.onLoadData + res[i].index1] = res[i].data || '|'; // res[i].data有可能为空
+							else
+								inputsDataObj[_this.onLoadData + res[i].index1] += res[i].data?'|' + res[i].data : '|'; */
+						}
+					_this.$emit('activeFc', inputsDataObj); // 把用户输入数据封装成对象输出给父级
+					_this.inputs_reSet(); //提交后重置验证码
+				})
+			},
+			inputs_reSet() { //提交后重置验证码
+				this.code = '';
+				this.userCode = '';
+				this.Igree = false;
+			},
+			chooseImg(index, picsIndex) { //选择图片
+				let _this = this
+				uni.chooseImage({
+					success: function(res) {
+						_this.$set(_this.picsObj, _this.onLoadData + index + _this.onLoadData + picsIndex, res.tempFilePaths[0]);
+					}
+				})
+			},
+			clearPic(index, picsIndex) { //清除图片
+				this.picsObj[this.onLoadData + index + this.onLoadData + picsIndex] = '';
+			}
+		},
+		components: {
+			uniIcon
+		}
+	}
+</script>
+
+<style scoped>
+	.input_title {
+		width: 25%;
+		margin-right: 5%;
+	}
+
+	.input_item {
+		width: 75%;
+	}
+
+	.pic_view {
+		position: relative;
+		height: 7vh;
+		width: 20vw;
+		border: 1px solid #f2f2f2;
+	}
+
+	.picsItem_pic {
+		height: 7vh;
+		width: 20vw;
+	}
+
+	/* width值与 .pic_view 相同*/
+	.picsItem_title {
+		width: 20vw;
+		font-size: 1.5vh;
+		color: #adadad;
+	}
+
+	.clearView {
+		position: absolute;
+		top: -2vh;
+		right: -1.5vh;
+		padding: .1vh;
+	}
+
+	.radioItem_name {
+		width: 15vw;
+	}
+	
+	/* 公共样式(可剪切至App.vue) */
+	.overflow_x_scroll{
+		overflow-x: scroll;
+	}
+	
+	.width100 {
+		width: 100%;
+	}
+
+	.flex_column {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.flex_column_c_c {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.flex_row {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.flex_row_none_c {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+
+	.flex_row_e_c {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-end;
+		align-items: center;
+	}
+
+	.flex_row_c_c {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.flex_row_between_c {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	.padding2vh3vw {
+		padding: 2vh 3vw;
+	}
+
+	.box-sizing-border-box {
+		box-sizing: border-box;
+	}
+
+	.fontSize1Point8vh {
+		font-size: 1.8vh;
+	}
+
+	.fontSize2Point1vh {
+		font-size: 2.1vh;
+	}
+
+	.fontColorF1505C {
+		color: #F1505C;
+	}
+
+	.fontColorADADAD {
+		color: #ADADAD;
+	}
+
+	.fontColor666666 {
+		color: #666666;
+	}
+
+	.fontColor007AFF {
+		color: #007AFF;
+	}
+
+	.padding1vh3vw {
+		padding: 1vh 3vw;
+	}
+
+	.padding1vh {
+		padding: 1vh;
+	}
+
+	.marginTop1vh {
+		margin-top: 1vh;
+	}
+
+	.borderBottom1pxf2f2f2 {
+		border-bottom: 1px solid #F2F2F2;
+	}
+
+	.margin2vh3vw {
+		margin: 2vh 3vw;
+	}
+	
+	.marginItemRight2vw {
+		margin-right: 2vw;
+	}
+</style>
---
+++ inputs_up.md
@@ -0,0 +1,205 @@
+## 作者想说
+
+如果该组件有什么问题还请大家说出来哦，还有如果有什么建议的话也可以提下呐 ~
+如果觉得好用，可以回来给个五星好评么~~(❁´◡`❁)*✲ﾟ*  蟹蟹~拜托啦~
+
+## 更新说明
+
+| 序号 | 更新说明 |
+|---|------|
+| 5 | ruleName属性修改为ruleArray, 可以支持一个以上的规则或协议 |
+| 4 | 新增radio(单选)类型，checkbox（多选）类型 |
+| 3 | 为提升用户体验，在循环项数较多的情况下，防止超屏，新增overflow_x为scroll(x轴滚动) |
+| 2 | 判断类型使用type判断 |
+| 1 | 完善213-226行的判断写法不正确问题 |
+
+# inputs组件使用说明
+注：有引入官方的uni-Icon组件（删除图片的叉叉），可自行修改
+	  `单位使用为vh、vw`， 有样式需求自行修改
+
+## html中使用
+
+```html
+<template>
+  <view>
+	<inputs :inputs="inputsArray" activeName="申请入驻" :ifCode="true" 
+			:ifRule="true" :ruleArray="ruleArray" 
+			v-on:chaildOpenEvent="openWin" 
+			v-on:activeFc="activeFc"  :onLoadData="onLoadData" />
+  </view>
+</template>
+```
+## JS中引入、注册并使用组件
+```javascript
+<script>
+  import inputs from /*inputs组件文件路径；*/
+  export default {
+    data() {
+      return {
+	    inputsArray: [{
+			type: 'radio',
+			title: 'radioName',
+			radioArray: [{
+				name: '测试一',
+				value: '测试一值'
+			}, {
+				name: '测试二',
+				value: '测试二值'
+			}]
+		},{
+			type: 'checkbox',
+			title: 'checkboxName',
+			checkboxArray: [{
+				name: '测试一',
+				value: '测试一值'
+			}, {
+				name: '测试二',
+				value: '测试二值'
+			}]
+		},{
+			title: '商家名称'
+		},{
+			title: '商家地址'
+		},{
+			type: 'pics',
+			title: '营业执照',
+			picsArray: [{title: '营业执照(三合一)'}]
+		},{
+			title: '银行名称'
+		},{
+			title: '银行账户'
+		},{
+			title: '开户人'
+		},{
+			title: '身份证号'
+		},{
+			type: 'pics',
+			title: '身份证截图',
+			picsArray: [{title: '身份证正面'}, {title: '身份证背面'}]
+		},{
+			type: 'pics',
+			title: '门店图片',
+			picsArray: [
+                         {title: '门头照'}, 
+                         {title: '店内样式1', ignore: true}, 
+                         {title: '店内样式2', ignore: true}]
+		},{
+			title: '商家折扣'
+		},{
+			title: '手机号',
+			phone: true
+		}]，
+		ruleArray: [{
+			name:'用户协议',
+			value: 'yonghuxieyi'
+		},{
+			name:'隐私政策',
+			value: 'yinsizhengce'
+		}],
+		onLoadData： 'data_'	//获取数据时变量名前缀
+      };
+    },
+    methods: {
+      openWin(value) {
+        //打开规则或协议页
+		//若有一个以上的rule，则根据value打开规则页面
+		console.log(value);
+      },
+      activeFc(res) {	// 最终取值
+        //主方法，携带用户输入的数据对象
+        let _this = this;
+        console.log(JSON.stringify(res));
+        // 取值为 this.onloadData + index, onloadData默认值为'data_'
+        submitFc(res[_this.onLoadData + '0'], res[_this.onLoadData + '1'], ……, function(result) {
+          console.log(JSON.stringify(result))
+          if(result.data && result.data == 'success')
+            _app.showToast('申请发送成功');
+          else
+            _app.showToast('出了些问题:' + JSON.stringify(result));
+          }, function(err) {
+            _app.showToast('申请发送失败:' + JSON.stringify(err));
+          });
+        }
+      },
+    components: {inputs}
+  }
+</script>
+```
+## 传给inputs组件的属性
+| 属性名| 是否必填| 值类型| 默认值| 说明|
+|------|---|----|---|-------|
+| inputs| 是| Array| []| 需循环的inputs数组|
+| activeName| 是| String| '发送'| 主功能按钮的文字说明|
+| ifCode| 否| Boolean| false| 是否启用验证码功能, 若启用则需完善163-168的发送验证码方法|
+| ifRule| 否| Boolean| false| 是否需要用户同意某规则或协议|
+| ruleArray| ifRule为true时是| Array| []| 需要用户同意某规则或协议的数组|
+| v-on:chaildOpenEvent| ifRule为true时是| Function| | 打开某规则或协议的方法|
+| v-on:activeFc| 是| Function| | 主功能方法，携带一个用户所输入的数据对象|
+| onLoadData| 否| String| 'data_'| activeFc返回的对象中的数据变量名前缀，后面跟index，看下方说明|
+| fontSize| 否| Number| '2.1' | title的文字大小|
+| fontColor| 否| String| '#666666'| title的文字颜色|
+| input_fontSize| 否| Number| '2.1'| input的文字大小|
+
+### ruleArray属性说明
+| 属性 | 说明|
+|---|---|
+| name| 需要用户同意某规则或协议的名字 |
+| value| 需要用户同意某规则或协议的标识(父级方法判断用) |
+
+
+## inputs属性说明
+
+### 一、input
+| 属性名| 是否必填| 值类型| 默认值| 说明|
+|------|---|----|---|-------|
+| type| 否| String| ''| 不传该值(因默认为input)|
+| title| 是| String| ''| 该项input的标题|
+| placeholder| 否| String| '请输入' + this.title| input的placeholder文字|
+| ignore| 否| Boolean| false| 是否可忽略该项（判断时可以为空）|
+| nullErr| 否| String| this.title + '不能为空'| 为空时提示|
+| inputType| 否| String| 'text'| 该项input的类型|
+| phone| 否| Boolean| false| 是否设此项为手机号input(判断时，判断此属性)|
+### 二、上传图片
+| 属性名| 是否必填| 值类型| 默认值| 说明|
+|------|---|----|---|-------|
+| type| 是| String| ''| 传固定值 type: 'pics'|
+| picsArray| 是| Array| []| 循环的图片数组，下方说明|
+| title| 是| String| ''| 该项图片的标题|
+| ignore| 否| Boolean| false| 是否可忽略该项（判断时可以为空）|
+#### picsArray属性说明
+| 属性名| 是否必填| 值类型| 默认值| 说明|
+|------|---|----|---|-------|
+| title| 否| String| ''| 该项图片的标题|
+| nullErr| 否| String| this.title + '不能为空'| 为空时提示|
+| ignore| 否| Boolean| false| 可以为空， 不判断是否为空,默认为必填，必填则在title前面有 * 标识|
+### 三、radio(单选)
+| 属性名| 是否必填| 值类型| 默认值| 说明|
+|------|---|----|---|-------|
+| type| 是| String| ''| 传固定值 type: 'radio'|
+| title| 是| String| ''| 该项radio的标题|
+| radioArray| 是| Array| []| 需循环的radio数组|
+| ignore| 否| Boolean| false| 是否可忽略该项（判断时可以为空）|
+| nullErr| 否| String| this.title + '不能为空'| 为空时提示|
+#### radioArray属性说明
+| 属性名| 是否必填| 值类型| 默认值| 说明|
+|------|---|----|---|-------|
+| name| 是| String| ''| 该radio的标题|
+| value| 是| | | 该项radio的值|
+### 四、checkbox(多选)
+| 属性名| 是否必填| 值类型| 默认值| 说明|
+|------|---|----|---|-------|
+| type| 是| String| ''| 传固定值 type: 'checkbox'|
+| title| 是| String| ''| 该项checkbox的标题|
+| checkboxArray| 是| Array| []| 需循环的checkbox数组|
+| ignore| 否| Boolean| false| 是否可忽略该项（判断时可以为空）|
+| nullErr| 否| String| this.title + '不能为空'| 为空时提示|
+#### checkboxArray属性说明
+| 属性名| 是否必填| 值类型| 默认值| 说明|
+|------|---|----|---|-------|
+| name| 是| String| ''| 该checkbox的标题|
+| value| 是| | | 该项checkbox的值|
+
+注： checkbox类型与radio类型差不多，只是取值时checkbox为数组，根据需求使用
+
+
+
